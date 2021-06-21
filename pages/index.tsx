f01dab116d39a0aa554ed29868c523dc235ba4fd
jfabraxas <github@jfa.gmbh>
2021-06-21 15:45:44 +0200

    1 file modified

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   const worker = React.useRef()
   React.useEffect(() => {
     if(!worker.current){
-      const load = async() => {
+      const load = async () => {
         worker.current = await spawn(
           new Worker(new URL("../worker/hello",import.meta.url))
           )
@@ -22,7 +22,6 @@ export default function Home() {
       <Head>
         <title>Create Next App</title>
       </Head>
-
       <main className={styles.main}>
         <h1 
           className={styles.title} 
@@ -34,10 +33,7 @@ export default function Home() {
             } >
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-
-        
       </main>
-
       <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
